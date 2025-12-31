15046c44836f9b881a61f9623b92a5fd828c0c3c
voov-v <yumin5482@gmail.com>
2025-12-31 17:58:49 +0900

    update : Player states

---
+++ text_rpg/source/Private/States/Player.cpp
@@ -0,0 +1,118 @@
+﻿#include "States/Player.h"
+
+
+Player::Player(int level = 1, int hp = 200, int attack = 30, int exp = 0)
+{
+    this->Level_ = level;
+    this->HP_ = hp;
+    this->Attack_ = attack;
+    this->Exp_ = exp;
+    this->MaxHP_ = hp;
+    this->Gold_ = 0;
+}
+
+Player::~Player()
+{
+    std::cout << "gg" << std::endl;
+}
+
+
+void Player::GainExp(int amount) 
+{
+    Exp_ += amount;
+}
+
+void Player::GainGold(int amount)
+{
+    Gold_ += amount;
+}
+
+bool Player::IsDead() const
+{
+    if (HP_ <= 0)
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+
+    // return HP_ <= 0; 챗지피티식
+}
+
+bool Player::TakeDamage(int dmg)
+{
+    HP_ -= dmg;
+    if (IsDead())
+    {
+        return true;
+    }
+}
+
+void Player::TryLevelUp()
+{   
+    if (Level_ < 10)
+    {
+        if (Exp_ >= 100)
+        {
+            Level_++;
+            MaxHP_ += Level_ * 20;
+            HP_ = MaxHP_;
+            Attack_ += Level_ * 5;
+        }
+    }
+}
+
+void Player::Init(std::string name)
+{
+    Name_ = name;
+}
+
+
+int Player::GetAttack()
+{
+    return Attack_;
+}
+
+bool Rand()
+{
+    return rand() % 2 == 0; 
+}
+
+void Player::IncreaseHP(int Value)
+{
+    HP_ += Value;
+}
+
+//void Player::ApplyItemEffect(Item item)
+//{
+//    if (HP_ <= 50) // 언제 쓸지 
+//        // 아이템 사용 조건
+//        // Item쪽 함수 호출
+//    {
+//        Itme->ApplyTo(ItemType type); 
+//    }
+//}
+
+//void Player::DecideBattleAction()
+//{
+//    if (Rand())
+//    {
+//        공격함수();
+//    }
+//    else
+//    {
+//        find(), ApplyItemEffect(Item item), remove() 
+//    }
+//}
+
+// 2분의 1확률로 공격 or 아이템 사용
+// 아이템 사용이면 아이템 있는지 여부, 조건으로 어떤 아이템을 사용할지
+// 아이템적용. 인벤토리에 있는 아이템을 제거
+
+// Invetory->find(); <<  
+// if 1번 hp, 2번 atk
+// ApplyItemEffect(Item item);
+// remove()
+
---
+++ text_rpg/source/Public/States/Player.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include "States/Item.h"
+#include "States/Inventory.h"
+#include <random>
+
+class Player 
+{
+public:
+    Player(int level, int hp, int attack, int exp);
+    
+    ~Player();
+    
+    // 이름 설정
+    void Init(std::string name);
+
+    // 공격 or 아이템 사용
+    void DecideBattleAction();   // enum?
+
+    // 피해를 받아 체력을 감소시킨다.
+    bool TakeDamage(int dmg);
+
+    // 체력이 0 이하인지 판단하여 사망 여부를 판정한다.
+    bool IsDead() const;
+
+    // 경험치 획득
+    void GainExp(int amount);
+
+    // 골드 획득
+    void GainGold(int amount);
+
+    // 레벨업, 스탯증가, 체력회복
+    void TryLevelUp();
+
+    // 아이템 효과 적용
+    /*void ApplyItemEffect(Item item);*/
+
+    // 공격
+    int GetAttack();
+
+    // 아이템 사용
+
+    // 랜덤
+    bool Rand();
+
+    void IncreaseHP(int Value);
+    
+    
+private:
+    /*Item* item;*/
+    std::string Name_;
+    int Level_;
+    int Exp_;
+    int Gold_;
+    int HP_;
+    int MaxHP_;
+    int Attack_;
+    /*Inventory* Inv_;*/
+};
