06de1254b09cc49791eefae3b370c04d0abf1244
HaNong <wonjong91@gmail.com>
2026-01-02 16:04:46 +0900

    [✨ update] BattleSystem.cpp 필수 기능에 필요한 함수들의 ui와 Item관련 쪽을 제외하고 구현
    bool StartBattle(Player* p, UIManager* ui) 함수 구현
    void ApplyRewards(Player* p, UIManager* ui) 함수 구현
    void TryDropItem(Player* p, UIManager* ui) 논리만 구현
    bool GetRandomBoolean(double probability) 플레이어 행동 결정과 몬스터 처치 시 아이템 획득 확률을 얻는 함수 구현

--- text_rpg/source/Private/Modes/BattleSystem.cpp
+++ text_rpg/source/Private/Modes/BattleSystem.cpp
@@ -6,15 +6,31 @@
 
 bool BattleSystem::StartBattle(Player* p, UIManager* ui)
 {
-    bool IsSomeoneDead = false;
-
+    bool bIsSomeoneDead = false;
     SpawnMonster(p);
-    while (!IsSomeoneDead)
+
+    // ui->PrintBattleStart(p, monster);
+
+    while (true)
     {
-        if (ResolveTurn(p, ui))
+        // 턴을 진행하고 몬스터나 플레이어가 사망하면
+        // if문에서 반목문 탈출
+        if (bIsSomeoneDead = ResolveTurn(p, ui))
         {
-            IsSomeoneDead = true;
+            break;
         }
+    }
+
+    // 몬스터 사망 시 보상 분배
+    if (monster_->isDead())
+    {
+        // ui->플레이어 승리 그리기();
+        ApplyRewards(p, ui);
+    }
+    // 플레이어 사망시 게임 종료
+    else if (p->IsDead())
+    {
+        // ui->PrintGameOver();
     }
 }
 
@@ -40,28 +56,31 @@ void BattleSystem::SpawnBoss(Player* p)
 bool BattleSystem::ResolveTurn(Player* p, UIManager* ui)
 {
     // 플레이어 선공격
-    if (IsAttack(ACTION_CHANCE))
+    if (DecideTurnAction(ACTION_CHANCE))
     {
         int damage = p->Attack();
-        // ui->공격 그리기();
         monster_->TakeDamage(damage);
-        // ui->데미지 받기그리기();
+        // ui->데미지 받기그리기(monster_);
 
         // 몬스터 사망시 함수 종료
         if (monster_->isDead())
         {
+            delete monster_;
             return true;
         }
     }
     else
-    {
-        p->UseItem();
-        // ui->아이템 사용 그리기();
+    { // 아이템 사용 파트, 버프 처리 체력 증가 처리 등
+        // ItemInfo witchItem = p->UseItem();
+        // ui->아이템 사용 그리기(ItemInfo);
     }
     
     // 몬스터 사망시 아래는 수행하지 않음
+
+
     int damage = monster_->Attack();
     p->TakeDamage(damage);
+    // ui->플레이어 데미지 받기 출력(p);
     if (p->IsDead())
     {
         return true;
@@ -72,13 +91,37 @@ bool BattleSystem::ResolveTurn(Player* p, UIManager* ui)
 
 void BattleSystem::ApplyRewards(Player* p, UIManager* ui)
 {
+    p->GainExp(EXP_REWARD);
+    // ui->PrintEXP(EXP_REWARD);
+    //if (bool bIsLevelUp = p->TryLevelUp())
+    //{
+    //    // ui->PrintLevelIP();
+    //}
+    int golds = GetRandomGold(GOLD_MIN, GOLD_MAX);
+
+    p->GainGold(golds);
+    // ui->PrintGainGold(golds);
+
+
+    TryDropItem(p, ui);
 }
 
 void BattleSystem::TryDropItem(Player* p, UIManager* ui)
 {
+    if (GetRandomBoolean(DROP_CHANCE))
+    {
+        // 아이템 리스트에서 아이템 받고
+        // ui->아이템획득 로그();
+        // 플레이어 인벤토리에 추가
+    }
 }
 
-bool BattleSystem::IsAttack(double probability)
+bool BattleSystem::DecideTurnAction(double probability)
+{
+    return GetRandomBoolean(ACTION_CHANCE);
+}
+
+bool BattleSystem::GetRandomBoolean(double probability)
 {
     static std::random_device rd;
     static std::mt19937 gen(rd());
