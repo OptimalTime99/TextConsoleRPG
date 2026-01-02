bf865286d46dc059189bda267cb14600b388229a
HaNong <wonjong91@gmail.com>
2026-01-02 14:28:31 +0900

    [✨ update] BattleSystemp.cpp에 ResolveTurn() 구현
    플레이어의 행동 결정을 0.5확률로 한 뒤 플레이어의 공격이나 아이템 사용 수행한 뒤 몬스터의 공격 수행. 플레이어나 몬스터의 공격 후 누군가 사망하면 true를 반환

--- text_rpg/source/Private/Modes/BattleSystem.cpp
+++ text_rpg/source/Private/Modes/BattleSystem.cpp
@@ -28,8 +28,46 @@ void BattleSystem::SpawnBoss(Player* p)
     monster_ = new Monster("Boss_test", p->GetLevel(), true);
 }
 
+// 플레이어 행동 결정 <- BattleSystem이
+// 아이템 사용 or 공격
+// if(공격)
+//  플레이어가 공격 -> 몬스터가 데미지 받기
+//  몬스터 사망했나?
+// if(아이템사용)
+//  플레이어 아이템 사용
+// 몬스터가 공격 -> 플레이어가 데미지 받기
+// 플레이어 사망했나?
 bool BattleSystem::ResolveTurn(Player* p, UIManager* ui)
 {
+    // 플레이어 선공격
+    if (GetRandom(ACTION_CHANCE))
+    {
+        int damage = p->Attack();
+        // ui->공격 그리기();
+        monster_->TakeDamage(damage);
+        // ui->데미지 받기그리기();
+
+        // 몬스터 사망시 함수 종료
+        if (monster_->isDead())
+        {
+            return true;
+        }
+    }
+    else
+    {
+        p->UseItem();
+        // ui->아이템 사용 그리기();
+    }
+    
+    // 몬스터 사망시 아래는 수행하지 않음
+    int damage = monster_->Attack();
+    p->TakeDamage(damage);
+    if (p->IsDead())
+    {
+        return true;
+    }
+
+    return false;
 }
 
 void BattleSystem::ApplyRewards(Player* p, UIManager* ui)
