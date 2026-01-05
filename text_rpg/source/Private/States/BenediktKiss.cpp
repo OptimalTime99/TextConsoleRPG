c7d8d3c76fdd1c727850b1608e38ec5a587258c2
zazang70-hue <zazang70@gmail.com>
2026-01-05 20:30:10 +0900

    [✨ update/이재현] 26/01/05
    Monster
    - BenediktKiss .cpp / .h 추가

---
+++ text_rpg/source/Private/States/BenediktKiss.cpp
@@ -0,0 +1,28 @@
+﻿#include "States/BenediktKiss.h"
+#include <string>
+#include <iostream>
+
+BenediktKiss::BenediktKiss(std::string Monstername_, int Level)
+    : Monster(Monstername_, Level)
+{
+    Monstername_ = "Benedikt Kiss";
+    GenerateStatus(Level);
+}
+
+void BenediktKiss::GenerateStatus(int Level)
+{
+    if (MonsterLevel_ == 0)
+    {
+        return;
+    }
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+
+    static std::uniform_int_distribution<int> DistHP(Level * MIN_HP, Level * MAX_HP);
+    static std::uniform_int_distribution<int> DistATK(Level * MIN_ATK, Level * MAX_ATK);
+
+
+    this->HP_ = DistHP(gen);
+    this->MaxHP_ = this->HP_;
+    this->Attack_ = DistATK(gen);
+}
