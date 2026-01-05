0c7e7b8267169a601157da97eeeed5ae2698eaae
zazang70-hue <zazang70@gmail.com>
2026-01-05 16:19:24 +0900

    [✨ update/ 이재현]  26/01/05
    Monster의 추상 클래스화 및 자식클래스 생성

---
+++ text_rpg/source/Private/States/BossMonster.cpp
@@ -0,0 +1,30 @@
+﻿#include "States/BossMonster.h"
+#include <string>
+#include <iostream>
+
+
+
+BossMonster::BossMonster(std::string Monstername_, int Level)
+    : Monster(Monstername_, Level)
+{
+    MonsterName_ = "BossMonster";
+    GenerateStatus(Level);
+}
+
+void BossMonster::GenerateStatus(int Level)
+{
+    if (MonsterLevel_ == 0)
+    {
+        return;
+    }
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+
+    static std::uniform_int_distribution<int> DistHP(Level * MIN_BOSS_HP, Level * MAX_BOSS_HP);
+    static std::uniform_int_distribution<int> DistATK(Level * MIN_BOSS_ATK, Level * MAX_BOSS_ATK);
+
+
+    this->HP_ = DistHP(gen);
+    this->MaxHP_ = this->HP_;
+    this->Attack_ = DistATK(gen);
+}
---
+++ text_rpg/source/Private/States/NormalMonster.cpp
@@ -0,0 +1,33 @@
+﻿#include "States/NormalMonster.h"
+#include <string>
+#include <iostream>
+
+
+
+NormalMonster::NormalMonster(std::string Monstername_, int Level)
+    : Monster(Monstername_, Level)
+{
+    Monstername_ = "NormalMonster";
+    GenerateStatus(Level);
+}
+
+void NormalMonster::GenerateStatus(int Level)
+{
+    if (MonsterLevel_ == 0)
+    {
+        return;
+    }
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+
+    static std::uniform_int_distribution<int> DistHP(Level * MIN_HP, Level * MAX_HP);
+    static std::uniform_int_distribution<int> DistATK(Level * MIN_ATK, Level * MAX_ATK);
+
+
+    this->HP_ = DistHP(gen);
+    this->MaxHP_ = this->HP_;
+    this->Attack_ = DistATK(gen);
+}
+
+
+
