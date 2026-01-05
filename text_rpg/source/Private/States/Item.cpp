d2985196a181bb28257281c47244a37039750585
OptimalTime99 <optimal9744@gmail.com>
2026-01-05 12:49:03 +0900

    [🩹 fix / 최준혁] Item 클래스 소스 파일 수정

--- text_rpg/source/Private/States/Item.cpp
+++ text_rpg/source/Private/States/Item.cpp
@@ -1,45 +1,69 @@
 ﻿#include "States/Item.h"
-#include <cassert>
+#include "Types/StatusType.h"
 
 
-Item::Item(const ItemDefinition* def) : def_(def)
+Item::Item(std::string name, int price) : name_(name), price_(price) {}
+
+void Item::AddEffect(StatusType status, int value)
 {
-    assert(def_ && "ItemDefinition is null.");
+    effect_[status] = value;
 }
 
-int Item::GetSellPrice(double sellRate) const
+void Item::Initialize()
 {
-    assert(def_);
+    // 하급 체력 포션 정의
+    Item HP_Potion("하급 체력 포션", 10);
+    HP_Potion.AddEffect(StatusType::HP, 50);
+    itemRegistry_["하급 체력 포션"] = HP_Potion;
 
-    if (sellRate <= 0)
+    // 하급 공격력 포션 정의
+    Item ATK_Potion("하급 공격력 포션", 15);
+    HP_Potion.AddEffect(StatusType::ATK, 10);
+    itemRegistry_["하급 공격력 포션"] = ATK_Potion;
+}
+
+const Item* Item::GetData(std::string name)
+{
+    if (itemRegistry_.find(name) != itemRegistry_.end())
     {
-        return def_->Price_;
+        return &itemRegistry_[name];
     }
 
-    return def_->Price_ * sellRate;
+    return nullptr; // 없는 아이템일 경우
 }
 
-const char* Item::GetName() const
+std::string Item::GetName() const
 {
-    assert(def_);
-    return def_->Name_;
-}
-
-
-ItemType Item::GetType() const
-{
-    assert(def_);
-    return def_->Type_;
-}
-
-int Item::GetValue() const
-{
-    assert(def_);
-    return def_->Value_;
+    return name_;
 }
 
 int Item::GetPrice() const
 {
-    assert(def_);
-    return def_->Price_;
+    return price_;
+}
+
+std::map<StatusType, int> Item::GetEffect() const
+{
+    return effect_;
+}
+
+void Item::SetName(const std::string& name)
+{
+    if (name_ == "") name_ = "이름 없음";
+
+    name_ = name;
+}
+
+void Item::SetPrice(int price)
+{
+    if(price < 0) price_ = 0;
+        
+    price_ = price;
+}
+
+void Item::SetEffect(const std::map<StatusType, int>& effect)
+{
+    if (effect.empty()) return;
+
+    effect_ = effect;
 }
