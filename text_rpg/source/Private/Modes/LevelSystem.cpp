7814aabe14b251aad790b0dc18a0ff1971206254
HaNong <wonjong91@gmail.com>
2026-01-05 20:25:03 +0900

    [🩹 fix] LevelUp 오류 수정
    간단 수정

--- text_rpg/source/Private/Modes/LevelSystem.cpp
+++ text_rpg/source/Private/Modes/LevelSystem.cpp
@@ -21,11 +21,11 @@ int LevelSystem::LevelUp(Player* p)
             Level++;
             p->SetLevel(Level);
             // 레벨업시 체력 증가
-            p->SetMaxHP(MaxHP + HP_PER_LEVEL));
+            p->SetMaxHP(MaxHP + HP_PER_LEVEL);
             // 현재 체력을 최대만큼 회복
             p->SetHP(MaxHP);
             // 레벨업시 공격력 증가    
-            p->SetAttack(Attack + ATTACK_PER_LEVEL));
+            p->SetAttack(Attack + ATTACK_PER_LEVEL);
             // 레벨업 후 남은 경험치
             p->SetExp(Exp - MaxExp);
             //몇번 레벨업했는지
