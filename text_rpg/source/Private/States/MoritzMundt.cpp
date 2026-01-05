93b65d399cd81a7311432c0cc295625f7f90b1e8
zazang70-hue <zazang70@gmail.com>
2026-01-05 20:26:17 +0900

    [✨ update/이재현] 26/01/05
    Monster
    - MoritzMundt .cpp / .h 추가

---
+++ text_rpg/source/Private/States/MoritzMundt.cpp
@@ -0,0 +1,28 @@
+﻿#include "States/MoritzMundt.h"
+#include <string>
+#include <iostream>
+
+MoritzMundt::MoritzMundt(std::string Monstername_, int Level)
+    : Monster(Monstername_, Level)
+{
+    Monstername_ = "Moritz Mundt";
+    GenerateStatus(Level);
+}
+
+void MoritzMundt::GenerateStatus(int Level)
+{
+    if (MonsterLevel_ == 0)
+    {
+        return;
+    }
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+
+    static std::uniform_int_distribution<int> DistHP(Level * MIN_HP, Level * MAX_HP);
+    static std::uniform_int_distribution<int> DistATK(Level * MIN_ATK, Level * MAX_ATK);
+
+
+    this->HP_ = DistHP(gen);
+    this->MaxHP_ = this->HP_;
+    this->Attack_ = DistATK(gen);
+}
