beedc069aa61d092bf2599f191a1416a15c80a95
voov-v <yumin5482@gmail.com>
2026-01-05 20:20:04 +0900

    [🩹 fix] Player 2026-1-2
    LevelUp 함수 수정

--- text_rpg/source/Private/Modes/LevelSystem.cpp
+++ text_rpg/source/Private/Modes/LevelSystem.cpp
@@ -16,16 +16,16 @@ int LevelSystem::LevelUp(Player* p)
     // 최대경험치보다 크거나 같을 시
     while (Level < MAX_LEVEL)
     {   // 레벨이 최대레벨보다 작을 때
-        if (p->GetExp >= MaxExp)
+        if (p->GetExp() >= MaxExp)
         {   
             Level++;
             p->SetLevel(Level);
             // 레벨업시 체력 증가
-            p->SetMaxHP(MaxHP + (Level * HP_PER_LEVEL));
+            p->SetMaxHP(MaxHP + HP_PER_LEVEL));
             // 현재 체력을 최대만큼 회복
             p->SetHP(MaxHP);
             // 레벨업시 공격력 증가    
-            p->SetAttack(Attack + (Level * ATTACK_PER_LEVEL));
+            p->SetAttack(Attack + ATTACK_PER_LEVEL));
             // 레벨업 후 남은 경험치
             p->SetExp(Exp - MaxExp);
             //몇번 레벨업했는지
