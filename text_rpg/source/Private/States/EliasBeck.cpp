bcb0c914475a4c490699033fa32004ab3b60afd8
zazang70-hue <zazang70@gmail.com>
2026-01-05 20:29:45 +0900

    [✨ update/이재현] 26/01/05
    Monster
    - EliasBeck .cpp / .h 추가

---
+++ text_rpg/source/Private/States/EliasBeck.cpp
@@ -0,0 +1,28 @@
+﻿#include "States/EliasBeck.h"
+#include <string>
+#include <iostream>
+
+EliasBeck::EliasBeck(std::string Monstername_, int Level)
+    : Monster(Monstername_, Level)
+{
+    Monstername_ = "Elias Beck";
+    GenerateStatus(Level);
+}
+
+void EliasBeck::GenerateStatus(int Level)
+{
+    if (MonsterLevel_ == 0)
+    {
+        return;
+    }
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+
+    static std::uniform_int_distribution<int> DistHP(Level * MIN_HP, Level * MAX_HP);
+    static std::uniform_int_distribution<int> DistATK(Level * MIN_ATK, Level * MAX_ATK);
+
+
+    this->HP_ = DistHP(gen);
+    this->MaxHP_ = this->HP_;
+    this->Attack_ = DistATK(gen);
+}
---
+++ text_rpg/source/Public/States/EliasBeck.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "States/Monster.h"
+#include "States/player.h"
+#include <string>
+#include <iostream>
+#include <random>
+
+class EliasBeck : public Monster
+{
+public:
+    EliasBeck(std::string Monstername_, int Level);
+
+    void GenerateStatus(int Level)override;
+
+};
