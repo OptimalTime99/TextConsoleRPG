c7ba8acd80208179414f7ca74265a8c4ff0e59cb
zazang70-hue <zazang70@gmail.com>
2026-01-05 16:17:23 +0900

    [🩹 fix/이재현] TakeDamage 및 Attack
    요청에 의한 Takedamage 코드 수정
    Attack 함수 추가

--- text_rpg/source/Public/States/Monster.h
+++ text_rpg/source/Public/States/Monster.h
@@ -5,18 +5,23 @@
 class Monster 
 {
 public:
-
-    // 몬스터 생성자
-    Monster(std::string MonsterName,int Level,bool bidBoos);
-
-    // 몬스터의 스텟 생성
-    void GenerateNomal(int Level);
     
-    // 보스 몬스터의 스텟 생성
-    void GenerateBoss(int Level);
+    Monster(std::string Monstername_, int level) 
+    {
+
+    }
+   
+    // 가상 소멸자
+    virtual ~Monster() 
+    {
+    
+    }
+  
+    // 몬스터의 스텟 생성
+    virtual void GenerateStatus(int Level_) = 0;
     
     // 데미지를 받는 로직
-    void TakeDamage(int dmg);
+    int TakeDamage(int dmg);
 
     // 사망 여부
     bool isDead()const;
@@ -29,29 +34,33 @@ public:
 
     // 공격 
     int GetAttack();
-
+ 
     // 몬스터 이름
     std::string GetName();
+  
     
     // 몬스터 레벨
     int GetLevel();
+  
 
-private:
+    int Attack();
+  
+
+protected:
     std::string MonsterName_;
     int MonsterLevel_;
     int HP_;
     int MaxHP_;
     int Attack_;
-    bool bisBoss_;
+
     
     // 상수로 만든 랜덤 스탯용 멤버 변수
-    //
     static constexpr int MIN_HP = 20;
     static constexpr int MAX_HP = 30;
     static constexpr int MIN_ATK = 5;
     static constexpr int MAX_ATK = 10;
-    static constexpr int BOSS_MIN_HP = 30;
-    static constexpr int BOSS_MAX_HP = 45;
+    static constexpr int MIN_BOSS_HP = 30;
+    static constexpr int MAX_BOSS_HP = 45;
     static constexpr int MIN_BOSS_ATK = 8;
     static constexpr int MAX_BOSS_ATK = 15;
 };
